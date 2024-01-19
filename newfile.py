732f205d59512831330ea61bfede8de6d36ccf9f
Divyanshichandani <144115520+Divyanshichandani@users.noreply.github.com>
2024-01-19 10:36:28 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,15 @@
+#wap to create your biodata using input functions
+a=input("name")
+b=int(input("age"))
+c=input("gender")
+d=input("college year")
+e=input("sap id")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+#dynamic typing
+x='python is'
+y=' awesome'
+print(x+y)
