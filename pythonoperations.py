e158f076827fbcddbcdd14bd42734679b2251d96
Divyanshichandani <144115520+Divyanshichandani@users.noreply.github.com>
2024-01-20 15:02:37 +0530

    Add files via upload

---
+++ pythonoperations.py
@@ -0,0 +1,25 @@
+print("all string functions in python")
+a= 'helloworld'
+b= '30'
+c='        this is python'
+print(a[1])
+print(a[2:5])
+print(b*3)
+print(a.capitalize()) #capitalizes first letter
+print(a.lower()) #converts to lowercase
+print(a.upper()) #converstes to uppercase
+print(a.isalnum()) #checks whether a string 
+print(b.isalpha())
+print(a.find('l'))
+print(a.endswith('d'))
+print(b.endswith('3'))
+print(a.isupper())
+print(len(a))
+print(a.islower())
+print(c.strip()) #removes unecessary spaces
+print(a.count('l')) #checks the number of times a letter has been repeated
+print(a.isascii())
+print(a.isprintable())
+print(a.isspace()) #to check if the string has spaces
+print(a.rsplit())
+print(c.partition('i'))
